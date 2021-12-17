2130a650e39a2b0dc8fa59ef165f44aed46825da
Theo Belo <theo.belo25@gmail.com>
2021-12-16 21:12:17 -0500

    built user models, created auth and signup api

---
+++ models/FollowerModel.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const FollowerSchema = new Schema(
+    {
+        user: { type: Schema.Types.ObjectId, ref: 'user' },
+        followers: [{
+            user: { type: Schema.Types.ObjectId, ref: 'user'},
+        }],
+        following: [{
+            user: { type: Schema.Types.ObjectId, ref: 'user'},
+        }],
+    }
+);
+
+module.exports = mongoose.model('follower', FollowerSchema);
